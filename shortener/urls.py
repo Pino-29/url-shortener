0ce1a8d83fac8b763c6ac3b323eb499c293e8c8d
Pino-29 <roberto.g.pino@gmail.com>
2025-10-09 19:52:52 -0600

    add views' endpoints

---
+++ shortener/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'shortener'
+
+urlpatterns = [
+    path('v1/urls/', views.create_short_url, name='create_short_url'),
+    path('v1/urls/<str:short_code>/', views.redirect_to_original, name='redirect_to_original'),
+]
